61e03e282eb2b636e9828f0582b75ba4f848b889
Kamil <kamil2001p@gmail.com>
2022-11-21 21:11:14 +0100

    Divided the data

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import time
 start_time = time.time()
 
+
+#-----odczytanie, pobranie i przetworzenie danych------#
 database = "wordbases/slownik.txt"
 
 keyletter = open("input/input.txt", "r", encoding='windows-1250')
 keyletter = keyletter.read(1)
 
-wordbase = open(database, "r", encoding='windows-1250')
-for x in wordbase:
+read = open(database, "r", encoding='windows-1250')
+wordbank = []   #zawiera wszystkie slowa
+reversedWordbank = [] #zawiera wszystkie slowa odwrocone
+keywords = []   #zawiera same slowa zaczynajace sie na keyletter
+for x in read:
     x = x.strip()
+    wordbank.append(x)
     if x.startswith(keyletter):
-        print(x)
+        keywords.append(x)
+
+for x in wordbank:
+    reversedWordbank.append(x[::-1])
+
+#----------------------------------------------------------#
 
 print(" ")
 print("--- s% seconds ---" % (time.time() - start_time))
