34465172adb0b7f56c8981d6a4cef5d703331499
Kamil <kamil2001p@gmail.com>
2022-11-21 12:54:57 +0100

    Implemented reading from wordbases

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import time
 start_time = time.time()
 
+database = "wordbases/slownik.txt"
+
+
+wordbase = open(database, "r", encoding='windows-1250')
+for x in wordbase:
+    print(x.strip())
+
+
+
+
+
 print(" ")
 print("--- s% seconds ---" % (time.time() - start_time))
