a3a66902d94780f08f7ab05ca0e0fe5fbb8753ab
Kamil <kamil2001p@gmail.com>
2022-11-21 13:25:59 +0100

    Implemented printing words starting from given letter

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ start_time = time.time()
 
 database = "wordbases/slownik.txt"
 
+keyletter = open("input/input.txt", "r", encoding='windows-1250')
+keyletter = keyletter.read(1)
 
 wordbase = open(database, "r", encoding='windows-1250')
 for x in wordbase:
-    print(x.strip())
-
-
-
-
+    x = x.strip()
+    if x.startswith(keyletter):
+        print(x)
 
 print(" ")
 print("--- s% seconds ---" % (time.time() - start_time))
