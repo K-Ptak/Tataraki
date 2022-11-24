1cc4e9b8310e4066b84d1bdf7f8e33a3116817d9
Kamil <kamil2001p@gmail.com>
2022-11-24 09:46:36 +0100

    Cleared the code

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ def reverse(x):
     return x[::-1]
 
 #-----odczytanie, pobranie i przetworzenie danych------#
-database = "wordbases/test.txt"
+database = "wordbases/slownik.txt"
 
 keyletter = open("input/input.txt", "r", encoding='windows-1250')
 keyletter = keyletter.read(1)
 
 read = open(database, "r", encoding='windows-1250')
+
 wordbank = []   #zawiera wszystkie slowa
 reversedWordbank = [] #zawiera wszystkie slowa odwrocone
 keywords = []   #zawiera same slowa zaczynajace sie na keyletter
 for x in read:
     x = x.strip()
-    wordbank.append(x)
-    if x.startswith(keyletter):
-        keywords.append(x)
+    if len(x) > 1:
+        wordbank.append(x)
+        if x.startswith(keyletter):
+            keywords.append(x)
 
 for x in wordbank:
     reversedWordbank.append(reverse(x))
@@ -27,7 +29,7 @@ for x in wordbank:
 for fullword in keywords:
     for firstword in keywords:
         for secondword in reversedWordbank:
-            if firstword in fullword and len(firstword) > 1 and len(secondword) > 1 and len(fullword) > 1 and len(fullword) - len(firstword) > 1:
+            if firstword in fullword and len(fullword) - len(firstword) > 1:
                 tempword = fullword.replace(firstword, '')
                 if secondword in tempword and len(secondword) == len(tempword):
                     print(f"{fullword} - {firstword} {reverse(secondword)}")
