0f7b7525f123fa8795904a01288779edd1fbbe8d
Kamil <kamil2001p@gmail.com>
2022-11-24 09:31:29 +0100

    Added working searching and printing algorithm on test data

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
 start_time = time.time()
-
+def reverse(x):
+    return x[::-1]
 
 #-----odczytanie, pobranie i przetworzenie danych------#
-database = "wordbases/slownik.txt"
+database = "wordbases/test.txt"
 
 keyletter = open("input/input.txt", "r", encoding='windows-1250')
 keyletter = keyletter.read(1)
@@ -19,9 +20,16 @@ for x in read:
         keywords.append(x)
 
 for x in wordbank:
-    reversedWordbank.append(x[::-1])
+    reversedWordbank.append(reverse(x))
 
 #----------------------------------------------------------#
 
+for fullword in keywords:
+    for firstword in keywords:
+        for secondword in reversedWordbank:
+            if firstword in fullword and len(firstword) > 1 and len(secondword) > 1 and len(fullword) > 1 and len(fullword) - len(firstword) > 1:
+                tempword = fullword.replace(firstword, '')
+                if secondword in tempword and len(secondword) == len(tempword):
+                    print(f"{fullword} - {firstword} {reverse(secondword)}")
 print(" ")
 print("--- s% seconds ---" % (time.time() - start_time))
