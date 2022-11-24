45d97d079c24bbd6f208388ebd8dc2f925433789
Kamil <kamil2001p@gmail.com>
2022-11-24 16:09:32 +0100

    Added function to check if files exist

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import time
+
 start_time = time.time()
-def reverse(x):
-    return x[::-1]
 
-#-----odczytanie, pobranie i przetworzenie danych------#
-database = "wordbases/slownik.txt"
 
-keyletter = open("input/input.txt", "r", encoding='windows-1250')
-keyletter = keyletter.read(1)
+def reverse(word):
+    return word[::-1]
 
-read = open(database, "r", encoding='windows-1250')
 
-wordbank = []   #zawiera wszystkie slowa
-reversedWordbank = [] #zawiera wszystkie slowa odwrocone
-keywords = []   #zawiera same slowa zaczynajace sie na keyletter
+def missingfile(filename):
+    if filename == "input":
+        print("Nie znaleziono pliku input")
+        exit()
+    if filename == "dictionary":
+        print("Nie znaleziono pliku slownikowego")
+        exit()
+
+
+# -----odczytanie, pobranie i przetworzenie danych------#
+
+database = "wordbases/.txt"
+input = "input/input.txt"
+
+try:
+    keyletter = open(input, "r", encoding='windows-1250')
+    keyletter = keyletter.read(1)
+except:
+    missingfile("input")
+
+try:
+    read = open(database, "r", encoding='windows-1250')
+except:
+    missingfile("dictionary")
+
+wordbank = []  # zawiera wszystkie slowa
+reversedWordbank = []  # zawiera wszystkie slowa odwrocone
+keywords = []  # zawiera same slowa zaczynajace sie na keyletter
 for x in read:
     x = x.strip()
     if len(x) > 1:
@@ -24,7 +45,7 @@ for x in read:
 for x in wordbank:
     reversedWordbank.append(reverse(x))
 
-#----------------------------------------------------------#
+# ----------------------------------------------------------#
 
 for fullword in keywords:
     for firstword in keywords:
