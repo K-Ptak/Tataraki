ade59a26304ce08daab879d34ad67d61fbaf2113
Kamil <kamil2001p@gmail.com>
2022-11-30 10:38:56 +0100

    Reconfigured algorithm to work on all input files in directory

--- tataraki.py
+++ tataraki.py
@@ -1,4 +1,6 @@
 import time
+from os import listdir
+from os.path import isfile, join
 
 start_time = time.time()
 
@@ -20,46 +22,56 @@ def errorcodes(errorname):
 
 
 # -----odczytanie, pobranie i przetworzenie danych------#
-
 database = "wordbases/test.txt"
-input = "input/input.txt"
-
-try:
-    keyletter = open(input, "r", encoding='windows-1250')
-    keyletter = keyletter.read(1)
-    try:
-        keyletter.isalnum()
-    except:
-        errorcodes("keyletter")
-except:
-    errorcodes("input")
+wordbank = []  # zawiera wszystkie slowa
+reversedWordbank = []  # zawiera wszystkie slowa odwrocone
 
 try:
     read = open(database, "r", encoding='windows-1250')
 except:
     errorcodes("dictionary")
 
-wordbank = []  # zawiera wszystkie slowa
-reversedWordbank = []  # zawiera wszystkie slowa odwrocone
-keywords = []  # zawiera same slowa zaczynajace sie na keyletter
 for x in read:
     x = x.strip()
     if len(x) > 1:
         wordbank.append(x)
-        if x.startswith(keyletter):
-            keywords.append(x)
 
 for x in wordbank:
     reversedWordbank.append(reverse(x))
 
-# ----------------------------------------------------------#
-for fullword in keywords:
-    for firstword in keywords:
-        for secondword in reversedWordbank:
-            if firstword in fullword and len(fullword) - len(firstword) > 1:
-                tempword = fullword.replace(firstword, '')
-                if secondword in tempword and len(secondword) == len(tempword):
-                    outputfile = open("output/output.txt", "w")
-                    outputfile.write(f"{fullword} - {firstword} {reverse(secondword)}")
+inputfiles = [file for file in listdir("input") if isfile(join("input", file))]
+for file in range(len(inputfiles)):
+
+
+    input = f"input/{inputfiles[file]}.txt"
+
+    try:
+        keyletter = open(input, "r", encoding='windows-1250')
+        keyletter = keyletter.read(1)
+        try:
+            keyletter.isalnum()
+        except:
+            errorcodes("keyletter")
+    except:
+        errorcodes("input")
+
+    keywords = []  # zawiera same slowa zaczynajace sie na keyletter
+    for x in wordbank:
+        if x.startswith(keyletter):
+                keywords.append(x)
+
+    for x in wordbank:
+        reversedWordbank.append(reverse(x))
+
+    # ----------------------------------------------------------#
+    for fullword in keywords:
+        for firstword in keywords:
+            for secondword in reversedWordbank:
+                if firstword in fullword and len(fullword) - len(firstword) > 1:
+                    tempword = fullword.replace(firstword, '')
+                    if secondword in tempword and len(secondword) == len(tempword):
+                        outputfile = open(f"output/output{file}.txt", "w")
+                        outputfile.write(f"{fullword} - {firstword} {reverse(secondword)}")
+
 print(" ")
 print("--- s% seconds ---" % (time.time() - start_time))
